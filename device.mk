31a3d71a4e8aa58514d916b695936ef5399c75b6
Amir Zaidi <azaidi@live.nl>
2017-08-14 13:48:20 +0100

    op3: Enable 60fps with camera blobs from OB18

--- device.mk
+++ device.mk
@@ -290,6 +290,10 @@ PRODUCT_PACKAGES += \
     Substratum
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuild/lib/libcameraservice.so:system/lib/libcameraservice.so \
+    $(LOCAL_PATH)/prebuild/lib/libmediaplayerservice.so:system/lib/libmediaplayerservice.so \
+    $(LOCAL_PATH)/prebuild/lib64/libcameraservice.so:system/lib64/libcameraservice.so \
+    $(LOCAL_PATH)/prebuild/lib64/libmediaplayerservice.so:system/lib64/libmediaplayerservice.so \
     $(LOCAL_PATH)/prebuild/lib64/libfilter-sdk.so:system/lib64/libfilter-sdk.so \
     $(LOCAL_PATH)/prebuild/lib64/libopbaselib.so:system/lib64/libopbaselib.so \
     $(LOCAL_PATH)/prebuild/lib64/libopcamera.so:system/lib64/libopcamera.so \
