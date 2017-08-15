3967e84f893f36cc135d440b3cb1aa0f6f04e1bb
siankatabg <siankata91@gmail.com>
2017-08-15 17:54:24 +0100

    Device: Disable 60fps in OnePlus Camera
    Doesn't work on this ROM and lead to crashes. Disabled till I find a fix.
    
    Change-Id: I930ef819985d6509ee68a716cf877c1ff3784147

--- device.mk
+++ device.mk
@@ -291,13 +291,14 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuild/lib/libcameraservice.so:system/lib/libcameraservice.so \
-    $(LOCAL_PATH)/prebuild/lib/libmediaplayerservice.so:system/lib/libmediaplayerservice.so \
     $(LOCAL_PATH)/prebuild/lib64/libcameraservice.so:system/lib64/libcameraservice.so \
-    $(LOCAL_PATH)/prebuild/lib64/libmediaplayerservice.so:system/lib64/libmediaplayerservice.so \
     $(LOCAL_PATH)/prebuild/lib64/libfilter-sdk.so:system/lib64/libfilter-sdk.so \
     $(LOCAL_PATH)/prebuild/lib64/libopbaselib.so:system/lib64/libopbaselib.so \
     $(LOCAL_PATH)/prebuild/lib64/libopcamera.so:system/lib64/libopcamera.so \
     $(LOCAL_PATH)/prebuild/lib64/libopcameralib.so:system/lib64/libopcameralib.so
+
+#    $(LOCAL_PATH)/prebuild/lib/libmediaplayerservice.so:system/lib/libmediaplayerservice.so \
+#    $(LOCAL_PATH)/prebuild/lib64/libmediaplayerservice.so:system/lib64/libmediaplayerservice.so \
 
 # QMI
 PRODUCT_PACKAGES += \
