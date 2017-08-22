5cfdb96acced91249c01062a4ffca14351b190d3
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:14:14 +0200

    oneplus3: Build Sensors HIDL multiHAL
     * Drop legacy HAL as no longer needed afterwards
    
    Change-Id: I42e8890f33f925b16c55169766acfef02034a0c5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -195,9 +195,6 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/oneplus/oneplus3
 #include device/qcom/sepolicy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
-# Sensors
-USE_SENSOR_MULTI_HAL := true
-
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAS_QCOM_WLAN_SDK := true
--- device.mk
+++ device.mk
@@ -344,7 +344,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    sensors.msm8996
+    android.hardware.sensors@1.0-impl
 
 # WiFi HAL
 PRODUCT_PACKAGES += \
