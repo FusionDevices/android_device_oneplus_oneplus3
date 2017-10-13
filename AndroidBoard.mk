374e8e629fe2af3ad39d08c7897ed9a96df390bd
INT3NSE07 <d.jonathanpaul@gmail.com>
2017-10-14 03:01:36 +0530

    Revert "oneplus3: Update firmware flashing extension for OP3T"
    This reverts commit 15202c00d8018c06c9280c600c5f33be61436e11.

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -19,67 +19,39 @@ include $(CLEAR_VARS)
 # Set the firmware path in the environment
 target_firmware_path := $(ANDROID_BUILD_TOP)/vendor/oneplus/oneplus3/firmware_images/
 
-# Oneplus 3
-# static_nvbk file
-$(call add-firmware-file,$(target_firmware_path)/static_nvbk.bin-op3)
-# Radio fusion file
-$(call add-firmware-file,$(target_firmware_path)/NON-HLOS.bin-op3)
-# Bluetooth file
-$(call add-firmware-file,$(target_firmware_path)/BTFM.bin-op3)
-# rpm image
-$(call add-firmware-file,$(target_firmware_path)/rpm.mbn-op3)
-# pmic file
-$(call add-firmware-file,$(target_firmware_path)/pmic.elf-op3)
-# tz version image
-$(call add-firmware-file,$(target_firmware_path)/tz.mbn-op3)
-# hyp image
-$(call add-firmware-file,$(target_firmware_path)/hyp.mbn-op3)
-# adspso file
-$(call add-firmware-file,$(target_firmware_path)/adspso.bin-op3)
-# cmnlib image
-$(call add-firmware-file,$(target_firmware_path)/cmnlib.mbn-op3)
-# cmnlib64 image
-$(call add-firmware-file,$(target_firmware_path)/cmnlib64.mbn-op3)
-# devcfg image
-$(call add-firmware-file,$(target_firmware_path)/devcfg.mbn-op3)
-# keymaster image
-$(call add-firmware-file,$(target_firmware_path)/keymaster.mbn-op3)
-# xbl file
-$(call add-firmware-file,$(target_firmware_path)/xbl.elf-op3)
-# bootloader image
-$(call add-firmware-file,$(target_firmware_path)/emmc_appsboot.mbn-op3)
-# lksecapp image
-$(call add-firmware-file,$(target_firmware_path)/lksecapp.mbn-op3)
+# Setup shipping the bootloader
+TARGET_BOOTLOADER_EMMC_INTERNAL := $(target_firmware_path)/emmc_appsboot.mbn
+$(TARGET_BOOTLOADER_EMMC_INTERNAL): $(TARGET_BOOTLOADER)
+INSTALLED_FIRMWAREIMAGE_TARGET += $(TARGET_BOOTLOADER_EMMC_INTERNAL)
 
-# Oneplus 3T
 # static_nvbk file
-$(call add-firmware-file,$(target_firmware_path)/static_nvbk.bin-op3t)
+$(call add-firmware-file,$(target_firmware_path)/static_nvbk.bin)
 # Radio fusion file
-$(call add-firmware-file,$(target_firmware_path)/NON-HLOS.bin-op3t)
+$(call add-firmware-file,$(target_firmware_path)/NON-HLOS.bin)
 # Bluetooth file
-$(call add-firmware-file,$(target_firmware_path)/BTFM.bin-op3t)
+$(call add-firmware-file,$(target_firmware_path)/BTFM.bin)
 # rpm image
-$(call add-firmware-file,$(target_firmware_path)/rpm.mbn-op3t)
+$(call add-firmware-file,$(target_firmware_path)/rpm.mbn)
 # pmic file
-$(call add-firmware-file,$(target_firmware_path)/pmic.elf-op3t)
+$(call add-firmware-file,$(target_firmware_path)/pmic.elf)
 # tz version image
-$(call add-firmware-file,$(target_firmware_path)/tz.mbn-op3t)
+$(call add-firmware-file,$(target_firmware_path)/tz.mbn)
 # hyp image
-$(call add-firmware-file,$(target_firmware_path)/hyp.mbn-op3t)
+$(call add-firmware-file,$(target_firmware_path)/hyp.mbn)
 # adspso file
-$(call add-firmware-file,$(target_firmware_path)/adspso.bin-op3t)
+$(call add-firmware-file,$(target_firmware_path)/adspso.bin)
 # cmnlib image
-$(call add-firmware-file,$(target_firmware_path)/cmnlib.mbn-op3t)
+$(call add-firmware-file,$(target_firmware_path)/cmnlib.mbn)
 # cmnlib64 image
-$(call add-firmware-file,$(target_firmware_path)/cmnlib64.mbn-op3t)
+$(call add-firmware-file,$(target_firmware_path)/cmnlib64.mbn)
 # devcfg image
-$(call add-firmware-file,$(target_firmware_path)/devcfg.mbn-op3t)
+$(call add-firmware-file,$(target_firmware_path)/devcfg.mbn)
 # keymaster image
-$(call add-firmware-file,$(target_firmware_path)/keymaster.mbn-op3t)
+$(call add-firmware-file,$(target_firmware_path)/keymaster.mbn)
 # xbl file
-$(call add-firmware-file,$(target_firmware_path)/xbl.elf-op3t)
-# bootloader image
-$(call add-firmware-file,$(target_firmware_path)/emmc_appsboot.mbn-op3t)
+$(call add-firmware-file,$(target_firmware_path)/xbl.elf)
+# lksecapp image
+$(call add-firmware-file,$(target_firmware_path)/lksecapp.mbn)
 
 # Unset local variable
 target_firmware_path :=
